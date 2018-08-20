f11946630e115db8ffe7e4b7b919ccaa1a3cdac6
zw <80575654@qq.com>
2018-08-20 14:40:25 +0800

    udpate appid

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var config = {
     // 下面的地址配合云端 Demo 工作
     service: {
         host,
-        appid: 'wx74ea43dd24dd8874',
+        appid: 'wxf54ff9fa9c9796c2',
         // 登录地址，用于建立会话
         loginUrl: `${host}/weapp/login`,
 
